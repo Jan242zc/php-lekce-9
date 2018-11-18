fd0f23d7b6ebb8f264d24ff6f60add92d070feba
Jan242zc <>
2018-11-18 19:22:15 +0000

    cviceni

--- src/Controller/ConverterController.php
+++ src/Controller/ConverterController.php
@@ -20,6 +20,33 @@ class ConverterController extends AbstractController
      */
     public function convert(Request $request): Response
     {
-        return $this->render('converter/convert.html.twig', []);
+        $form = $this->createFormBuilder()
+            ->add('text', TextType::class) 
+            ->add('convert', ChoiceType::class, [
+                'choices' => [
+                    'Velka pismena' => 'velka',
+                    'Mala pismena' => 'mala'
+                ]
+            ])
+            ->add('prevest', SubmitType::class)
+            ->getForm();    
+        
+        $form->handleRequest($request);
+            
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            $text = $formData['text'];
+            $convert = $formData['convert'];
+            if ($convert === 'velka'){
+                $vysledek = strtoupper($text);
+            } else {
+                $vysledek = strtolower($text);
+            }
+        }
+
+        return $this->render('converter/convert.html.twig', [
+            'form' => $form->createView(),
+            'result' => $vysledek ?? null
+        ]);
     }
 }
